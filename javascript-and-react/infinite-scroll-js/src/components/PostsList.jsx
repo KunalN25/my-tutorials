6b760297b547f656147e55cf3e32dd8dccb1ecc9
KunalNk25 <kunal.nalawade@thecarousell.com>
2024-06-24 18:32:35 +0530

    small deps fix

--- javascript-and-react/infinite-scroll-js/src/components/PostsList.jsx
+++ javascript-and-react/infinite-scroll-js/src/components/PostsList.jsx
@@ -8,16 +8,16 @@ const PostsList = () => {
   const [loading, setLoading] = useState(false);
   const observer = useRef();
 
-  const loadMorePosts = async () => {
+  const loadMorePosts = useCallback(async () => {
     setLoading(true);
     const newPosts = await fetchPosts(page, 10);
     setPosts((prevPosts) => [...prevPosts, ...newPosts]);
     setLoading(false);
-  };
+  }, [page]);
 
   useEffect(() => {
     loadMorePosts();
-  }, [page]);
+  }, [loadMorePosts]);
 
   const lastPostElementRef = useCallback(
     (node) => {
@@ -37,7 +37,7 @@ const PostsList = () => {
 
   return (
     <div>
-      <h1>Infinite Scroll</h1>
+      <h1>Your Feed</h1>
       <ul>
         {posts.map((post, index) => (
           <li
